e4d7589e6ad083f374d975a8a03c1ef97976963c
Carlos Cruz Rangel <79823316+CarlosCruzRangel@users.noreply.github.com>
2022-09-26 21:50:45 -0500

    Agrega bat y remplaza imagenes

--- Practicas/Practica2/src/run.bat
+++ Practicas/Practica2/src/run.bat
@@ -1,4 +1,4 @@
-::@echo off 
+@echo off 
 echo             ,--.
 echo          ,--.  .--,`) )  .--,
 echo       .--,`) \( (` /,--./ (`
@@ -28,7 +28,9 @@ echo.
 
     ::RUTA DE COMPILACION
     ::Cambiar por la ruta donde se encuentre: Computacion-Concurrente\practicas\Practica2
-    ::cd C:\Users\josed\OneDrive\Documentos\concurrente\practicas\Computacion-Concurrente\Practicas\Practica2
+    :: DEMIAN cd C:\Users\josed\OneDrive\Documentos\concurrente\practicas\Computacion-Concurrente\Practicas\Practica2
+    :: Eiffel 
+    cd C:\Users\Carlos\Documents\GitHub\Computacion-Concurrente\practicas\Practica2
     ::COMANDO DE COMPILACION
     javac -d ./classes src/Main.java src/practica2/filtros/*.java src/practica2/matrices/*.java
 
@@ -40,20 +42,20 @@ echo.
     ::DEMOSTRACION DE FILTROS SECUENCIALES
     :inicio
     SET var=0
-    cls
-    echo ------------------------------------------------------------------------------
     echo.
+    echo ------------------------------------------------------------------------------
+    echo Este es un menu interectivo ingresa el numero de la opcion que te interese. 
    
-    echo  1    Opcion 1  
-    echo  2    Opcion 2  
-    echo  3    Opcion 3  
-    echo  4    Opcion 4  
-    echo  5    Opcion 5 
-    echo  6    Opcion 6
-    echo  7    Opcion 7 
-    echo  8    Opcion 8
-    echo  9    Opcion 9
-    echo  10    Opcion 10    
+    echo  1     Filtro Gray uno       1
+    echo  2     Filtro Gray dos       2
+    echo  3     Filtro Gray tres      3
+    echo  4     Filtro Gray cuatro    4   
+    echo  5     Filtro Alto Contraste 5 
+    echo  6     Filtro Componente RGB 6
+    echo  7     Filtro Blur uno       7 
+    echo  8     Filtro Blur dos       8
+    echo  9     Filtro Blur           9
+    echo  10    Filtro Sharpen        10    
     echo  11    Salir 
 
     SET /p var= ^> Seleccione una opcion [1-10]:
@@ -82,8 +84,19 @@ echo.
         echo.
         echo. Has elegido la opcion No. 1 Gray
         echo.
-        ::Aquí van las líneas de comando de tu opción
-        java -cp ./classes Main 100 imagenes/javatar.png 1 false 5
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+        java -cp ./classes Main %var1% imagenes/javatar%var3%.png 1 false %var2%
 
 
         color 08
@@ -92,11 +105,22 @@ echo.
     goto:inicio
 
     :op2
-        echo.
+               echo.
         echo. Has elegido la opcion No. 2 Gray
         echo.
-            ::Aquí van las líneas de comando de tu opción
-         java -cp ./classes Main 100 imagenes/javatar.png 2 false 5   
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+        java -cp ./classes Main %var1% imagenes/javatar%var3%.png 2 false %var2%
         color 09
         echo.
         pause
@@ -106,8 +130,19 @@ echo.
         echo.
         echo. Has elegido la opcion No. 3 Gray
         echo.
-        ::Aquí van las líneas de comando de tu opción
-        java -cp ./classes Main 100 imagenes/javatar.png 3 false 5
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+        java -cp ./classes Main %var1% imagenes/javatar%var3%.png 3 false %var2%
         color 0A
         echo.
         pause
@@ -117,8 +152,19 @@ echo.
         echo.
         echo. Has elegido la opcion No. 4 Gray
         echo.
-        ::Aquí van las líneas de comando de tu opción
-        java -cp ./classes Main 100 imagenes/javatar.png 4 false 5
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+         java -cp ./classes Main %var1% imagenes/javatar%var3%.png 4 false %var2%
         color 0B
         echo.
         pause
@@ -128,7 +174,19 @@ echo.
         echo.
         echo. Has elegido la opcion No. 5 Alto Contraste
         echo.
-        java -cp ./classes Main 100 imagenes/javatar.png 5 false 5
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+        java -cp ./classes Main %var1% imagenes/javatar%var3%.png 5 false %var2%
         color 0C
         echo.
         pause
@@ -138,7 +196,19 @@ echo.
         echo.
         echo. Has elegido la opcion No. 6 Componentes RGB
         echo.
-        java -cp ./classes Main 100 imagenes/javatar.png 6 false 5
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+         java -cp ./classes Main %var1% imagenes/javatar%var3%.png 6 false %var2%
         color 0C
         echo.
         pause
@@ -148,7 +218,16 @@ echo.
         echo.
         echo. Has elegido la opcion No. 7 Componentes Blur
         echo.
-        java -cp ./classes Main 100 imagenes/javatar.png 7 false 5
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+         java -cp ./classes Main %var1% imagenes/javatar%var3%.png 7 false %var2%
         color 0C
         echo.
         pause
@@ -158,7 +237,19 @@ echo.
         echo.
         echo. Has elegido la opcion No. 8 Componentes Blur
         echo.
-        java -cp ./classes Main 10 imagenes/javatar.png 8 false 5
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+        java -cp ./classes Main %var1% imagenes/javatar%var3%.png 8 false %var2%
         color 0C
         echo.
         pause
@@ -168,7 +259,20 @@ echo.
         echo.
         echo. Has elegido la opcion No. 9 Blur
         echo.
-        java -cp ./classes Main 10 imagenes/javatar.png 9 false 5
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+        
+        java -cp ./classes Main %var1% imagenes/javatar%var3%.png 9 false %var2%
         color 0C
         echo.
         pause
@@ -178,7 +282,19 @@ echo.
         echo.
         echo. Has elegido la opcion No. 10 Sharpen
         echo.
-        java -cp ./classes Main 10 imagenes/javatar.png 10 false 5
+        echo Ingresa un numero de la matriz de aplicacion solo acepta matriz de 10x10, 100x100, 1000x1000
+        echo si quieres una matriz de 10x10 entonces ingresa 10
+        echo si quieres una matriz de 100x100 entonces ingresa 100
+        echo si quieres una matriz de 1000x1000 entonces ingresa 1000
+        SET var1=0
+        SET /p var1= ^> ingresa tu numero [1-1000]:
+        echo Ingresa un numero de hilos Te recomendamos entre 1 y 1000 hilos
+        SET var2=0
+        SET /p var2= ^> ingresa tu numero [1 - 1000]:
+        echo Tenemos tres ejemplos de imagen con ¿cual quiereres tarabajar? ¿1 o 2 o 3?
+        SET var3=0
+        SET /p var3= ^> ingresa tu numero [1 o 2 o 3]:
+         java -cp ./classes Main %var1% imagenes/javatar%var3%.png 10 false %var2%
         color 0C
         echo.
         pause
@@ -186,11 +302,6 @@ echo.
 
     :salir
         @cls&exit
-
-
-
-    
-
 
  pause
 pause exit
