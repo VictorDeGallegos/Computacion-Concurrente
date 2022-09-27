b64d087f9ba21c1fa1d420a3b4054551a329c9ce
Victor Gallegos <316160456@ciencias.unam.mx>
2022-09-26 20:06:24 -0500

    Repara numero de hilos por 1 en secuencial

--- Practicas/Practica2/src/practica2/sumas/SumaSecuencial.java
+++ Practicas/Practica2/src/practica2/sumas/SumaSecuencial.java
@@ -1,17 +1,12 @@
 package practica2.sumas;
 
-import java.util.Scanner;
-
 public class SumaSecuencial {
 
     // Suma secuencial de los primeros n numeros usando hilos
     public static void main(String[] args) {
-        int n = 10; // 10 primeros numeros
-        // numero de hilos por Scanner
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce el numero de hilos: ");
-        int hilos = sc.nextInt();
         long inicio_tiempo = System.nanoTime();
+        int n = 10; // 10 primeros numeros
+        int hilos = 1; // 1 hilos
         // suma secuencial
         int suma_total = 0;
         int inicio = 0;
